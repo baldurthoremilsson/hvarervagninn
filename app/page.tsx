5bccf7784f86d68967fd697cf1147cbed4377cbe
Baldur Þór Emilsson <baldur@baldur.biz>
2024-05-01 22:49:32 +0000

    better query for stops

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,37 @@ const STRAETO_API = "https://straeto.is/graphql"
 const DEFAULT_LAT = 64.143303656234;
 const DEFAULT_LON = -21.9146771540298;
 
+const QUERY_STOPS = `
+fragment StopsResult on GtfsStop {
+  id
+  name
+  lat
+  lon
+  type
+  rotation
+  code
+  isTerminal
+  routes
+  alerts {
+    id
+    cause
+    effect
+    routes
+    title
+    text
+    dateStart
+    dateEnd
+  }
+}
+
+query Stops($date: String) {
+  GtfsStops(date: $date) {
+    results {
+      ...StopsResult
+    }
+  }
+}`;
+
 interface Coords {
   lat: number,
   lon: number,
@@ -29,6 +60,20 @@ type Stop = {
 
 const calcPos = (posA: Coords, posB: Coords) => Math.sqrt(Math.pow(posA.lat - posB.lat, 2) + Math.pow(posA.lon - posB.lon, 2))
 
+const fetchStops = () => {
+  const now = new Date();
+  return fetch(STRAETO_API, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      query: QUERY_STOPS,
+      variables: {
+        date: now.toISOString().slice(0, 10),
+      }
+    }),
+  });
+}
+
 export default function Home() {
   let [currentPos, setCurrentPos] = useState<Coords>({lat: DEFAULT_LAT, lon: DEFAULT_LON});
   let [stops, setStops] = useState<Array<Stop>>([]);
@@ -41,14 +86,7 @@ export default function Home() {
   }, [setCurrentPos]);
 
   useEffect(() => {
-    let params = new URLSearchParams({
-      operationName: "Stops",
-      variables: JSON.stringify({}),
-      extensions: JSON.stringify({"persistedQuery":{"version":1,"sha256Hash":"f08f0d5961220e45de84d6d2b77a6e21de36e37066d510fa6c0d6f1ce145a82f"}}),
-    })
-    fetch(`${STRAETO_API}?` + params, {
-      headers: {"Content-Type": "application/json"}
-    })
+    fetchStops()
       .then(response => response.json())
       .then(stopsData => stopsData.data.GtfsStops.results.sort((a: Coords, b: Coords) => calcPos(a, currentPos) - calcPos(b, currentPos)))
       .then(setStops)
